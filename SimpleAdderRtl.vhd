97d51cadf80630b8cc55a7e1f852bd64a8595665
Kristoffer Nordstroem <kristoffer.nordstroem@scs.ch>
2018-10-04 16:08:04 +0200

    ! compile errors

--- SimpleAdderRtl.vhd
+++ SimpleAdderRtl.vhd
@@ -20,7 +20,7 @@ entity SimpleAdder is
     InVld  : in  std_logic;
     a      : in  std_logic_vector(31 downto 0);
     b      : in  std_logic_vector(31 downto 0);
-    OutVld : in  std_logic;
+    OutVld : out std_logic;
     c      : out std_logic_vector(31 downto 0));
 
 end entity SimpleAdder;
@@ -30,15 +30,15 @@ architecture rtl of SimpleAdder is
 begin  -- architecture rtl
 
   p_add_numbers: process (Clk) is
-    variable temp : unsigned(32 downto 0);
+    variable temp : unsigned(31 downto 0);
   begin  -- process p_add_numbers
     if Clk'event and Clk = '1' then     -- rising clock edge
       if Clr = '1' then
         OutVld <= '0';
         c <= (others => '0');
       else
-        tmp := unsigned(a) + unsigned(b);
-        c <= std_logic_vector(tmp(31 downto 0));
+        temp := unsigned(a) + unsigned(b);
+        c <= std_logic_vector(temp(31 downto 0));
       end if;
     end if;
   end process p_add_numbers;
